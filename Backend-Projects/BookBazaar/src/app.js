f4c16597ef0b2c2bfbf5871a710745e1c5456daa
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-07-06 14:01:24 +0530

    add logger and global error handler

--- Backend-Projects/BookBazaar/src/app.js
+++ Backend-Projects/BookBazaar/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 app.use(
     cors({
@@ -16,5 +20,25 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(err.status || 500).json({
+        status: "error",
+        message: err.message || "Internal Server Error",
+        ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+    });
+});
+
+// API Routes
+
+// Handle 404
+app.use((_, res) => {
+    res.status(404).json({
+        status: "error",
+        message: "Route not found",
+    });
+});
 
 export default app;
