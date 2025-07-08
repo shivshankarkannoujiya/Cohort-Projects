ec24b60168f3894a5bccc6bf0fdf5bcff8f79c3e
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-07-08 12:20:47 +0530

    Add bcryptjs for password hashing and JWT for token generation; create controllers, middlewares, and routes for user, book, cart, order, payment, and review

--- Backend-Projects/BookBazaar/src/models/user.models.js
+++ Backend-Projects/BookBazaar/src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
     {
@@ -36,4 +38,45 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true },
 );
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function () {
+    try {
+        return jwt.sign(
+            {
+                _id: this._id,
+                email: this.email,
+                role: this.role,
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
+        );
+    } catch (error) {
+        console.error("ERROR WHILE GENERATING ACCESS TOKEN");
+    }
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+    try {
+        return jwt.sign(
+            {
+                _id: this._id,
+            },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
+        );
+    } catch (error) {
+        console.error("ERROR WHILE REFRESH ACCESS TOKEN");
+    }
+};
+
 export const User = mongoose.model("User", userSchema);
