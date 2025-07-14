6c18f41eec355d4b4e7531b3eee24bf51288b720
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-07-14 12:16:32 +0530

    Fix optional chaining for decodedToken in JWT verification

--- Backend-Projects/BookBazaar/src/middlewares/auth.middleware.js
+++ Backend-Projects/BookBazaar/src/middlewares/auth.middleware.js
@@ -15,7 +15,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
 
     try {
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        const user = await User.findById(decodedToken._id).select(
+        const user = await User.findById(decodedToken?._id).select(
             "-password -refreshToken",
         );
 
