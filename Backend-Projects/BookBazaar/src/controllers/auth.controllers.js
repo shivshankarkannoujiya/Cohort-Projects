f9c3b1204116715cbc001ed9af99443bc613d1d9
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-07-16 10:46:03 +0530

    Fix registration and login error handling, correct password validation, and update cookie clearing logic

--- Backend-Projects/BookBazaar/src/controllers/auth.controllers.js
+++ Backend-Projects/BookBazaar/src/controllers/auth.controllers.js
@@ -9,8 +9,8 @@ import jwt from "jsonwebtoken";
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password, role } = req.body;
 
-    if ([name, email, password].every((field) => field?.trim() === "")) {
-        throw new ApiError(401, "All fields are required");
+    if (!name || !email || !password) {
+        throw new ApiError(400, "All fields are required");
     }
 
     const existingUser = await User.findOne({ email: email.toLowerCase() });
@@ -18,7 +18,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError("User already Exist", 400);
     }
 
-    const user = User.create({
+    const user = await User.create({
         name,
         email,
         role,
@@ -42,7 +42,7 @@ const loginUser = asyncHandler(async (req, res) => {
         throw new ApiError(404, "User not found");
     }
 
-    const isPasswordValid = await user.isPassworCorrect(password);
+    const isPasswordValid = await user.isPasswordCorrect(password);
     if (!isPasswordValid) {
         throw new ApiError(401, "Invalid Credential");
     }
@@ -92,7 +92,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
             throw new ApiError(401, "Invalid refresh Toekn");
         }
 
-        if (incommingRefreshToken !== user?.refreshToken) {
+        if (!incommingRefreshToken !== user?.refreshToken) {
             throw new ApiError(401, "Invalid refresh Toekn");
         }
 
@@ -148,8 +148,8 @@ const logoutUser = asyncHandler(async (req, res) => {
 
     return res
         .status(200)
-        .clearCookie("accessToken", accessToken, cookieOptions)
-        .clearCookie("refreshToken", refreshToken, cookieOptions)
+        .clearCookie("accessToken", cookieOptions)
+        .clearCookie("refreshToken", cookieOptions)
         .json(new ApiResponse(200, {}, "User logged Out successfully"));
 });
 
