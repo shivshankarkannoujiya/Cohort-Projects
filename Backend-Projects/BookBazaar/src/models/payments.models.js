702b9c350e786d90a9efcc4b8e2ce4fad9aab43c
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-07-16 11:09:58 +0530

    Add user field to payment schema to associate payments with users

--- Backend-Projects/BookBazaar/src/models/payments.models.js
+++ Backend-Projects/BookBazaar/src/models/payments.models.js
@@ -13,6 +13,12 @@ const paymentSchema = new mongoose.Schema(
             required: true,
         },
 
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+
         amount: {
             type: Number,
             required: true,
